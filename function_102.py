2a07d9f76e581834e4368e684ad67977cf3cbc38
Wajn <wajnaq1@gmail.com>
2026-02-22 16:59:00 +0300

    lab 1 completed

---
+++ function_102.py
@@ -0,0 +1,9 @@
+def find_primes(start, end):
+    for num in range(start, end + 1):
+        if num > 1:
+            for i in range(2, num):
+                if num % i == 0:
+                    break
+            else:
+                print(num)
+find_primes(25, 50)
